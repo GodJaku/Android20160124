460eb6aecf2f29abdc68f30f99a77b3c52539c13
0101rgb <0101rgb@gmail.com>
2016-01-23 23:10:39 +0900

    initial commit

---
+++ app/src/main/java/com/rgb0101/android20160124/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rgb0101.android20160124;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
